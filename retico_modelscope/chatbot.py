538d55f4c51e8b3f3d9fb02422a7243c10635102
porterrigby <prigby.a@gmail.com>
2025-10-22 13:02:57 -0600

    Changes to load model from ModelScope, and infer using transformers.

--- retico_modelscope/chatbot.py
+++ retico_modelscope/chatbot.py
@@ -1,6 +1,8 @@
-from modelscope.pipelines import pipeline
+import torch
+from modelscope.hub.snapshot_download import snapshot_download
 from retico_core.abstract import AbstractModule, UpdateType, UpdateMessage
 from retico_core.text import SpeechRecognitionIU, TextIU
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
 
 
 class ChatbotModule(AbstractModule):
@@ -25,20 +27,30 @@ class ChatbotModule(AbstractModule):
             self,
             model_path,
             max_new_tokens=512,
-            temperature=0.7,
-            repetition_penalty=1.1,
+            temperature=0.2,
             top_p=0.9,
             **kwargs
     ):
         super().__init__(**kwargs)
-        self.chatbot = pipeline(
-            'chat',
-            model=model_path
+
+        model_dir = snapshot_download(model_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True, use_fast=True)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_dir,
+            torch_dtype=torch.float16,
+            device_map='auto',
+            trust_remote_code=True
         )
+        self.streamer = TextStreamer(self.tokenizer, skip_prompt=True, skip_special_tokens=True)
         self.max_new_tokens = max_new_tokens
         self.temperature = temperature
-        self.repetition_penalty = repetition_penalty
         self.top_p = top_p
+        self.messages = [
+            {
+                'role': 'system',
+                'content': 'You are a friendly chatbot who responds to questions.'
+            }
+        ]
 
     def process_update(self, update_message):
         for iu, ut in update_message:
@@ -51,29 +63,48 @@ class ChatbotModule(AbstractModule):
                 for unit in self.current_output:
                     last_commit_sentence += f"{unit.text} "
                 self.current_output = []
+                last_commit_sentence = last_commit_sentence.strip()
 
                 if len(last_commit_sentence) > 0:
                     self.process_prompt(last_commit_sentence, iu)
 
     def process_prompt(self, last_commit_sentence, iu):
-        response = self.chatbot(
-            last_commit_sentence.strip(),
-            max_new_tokens=self.max_new_tokens,
-            temperature=self.temperature,
-            repetition_penalty=self.repetition_penalty,
-            top_p=self.top_p
-        )
-        print("response:", response)
-        words = response['text'].split(' ')
+        self.messages.append({
+            'role': 'user',
+            'content': last_commit_sentence
+        })
+
+        inputs = self.tokenizer.apply_chat_template(
+            self.messages,
+            tokenize=True,
+            add_generation_prompt=True,
+            return_tensors="pt",
+        ).to(self.model.device)
+
+        with torch.no_grad():
+            outputs = self.model.generate(
+                inputs,
+                max_new_tokens=self.max_new_tokens,
+                temperature=self.temperature,
+                top_p=self.top_p,
+                do_sample=True,
+                streamer=self.streamer,
+            )
+
+        response = self.tokenizer.decode(outputs[0][inputs.shape[1]:], skip_special_tokens=True)
+        self.messages.append({
+            'role':'assistant',
+            'content': response
+        })
+        words = response.split()
+
         new_iu = None
         for word in words:
-            #print(word, end=' ', flush=True)
             new_iu = self.create_iu(iu)
             new_iu.payload = word
             um = UpdateMessage.from_iu(new_iu, UpdateType.ADD)
             self.append(um)
 
         if new_iu is not None:
-            #print('\n', flush=True)
             um = UpdateMessage.from_iu(new_iu, UpdateType.COMMIT)
             self.append(um)
